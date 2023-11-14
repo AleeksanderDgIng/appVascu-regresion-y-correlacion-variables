9242e0dadb817316d0a115062db977ecb9c54489
AleeksanderDgIng <alexander.fcd@gmail.com>
2023-11-14 02:57:28 -0500

    Avance 80% modulo predictivo interactivos graficos

--- controllers/analizar_correlacion.py
+++ controllers/analizar_correlacion.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib
+import plotly.express as px
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from scipy.stats import pearsonr
-import mysql.connector
 from models.db_connection import get_db_connection
 
 # Función para analizar la correlación entre las variables de una tabla
@@ -49,26 +49,40 @@ def analizar_correlacion(selected_table, selected_columns):
                                 variable1 = col1
                                 variable2 = col2
 
-                # Generar el mapa de calor con seaborn
-                plt.figure(figsize=(7, 5)) # Define el tamaño de la figura
-                sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', cbar=True, linewidths=0.5, fmt=".2f", annot_kws={"size": 12})
-                plt.title(f"Mapa de Calor de Correlación ({selected_table})", fontsize=18)
-                plt.xlabel("Variables", fontsize=11)
-                plt.ylabel("Variables", fontsize=11)
-                plt.xticks(fontsize=11)
-                plt.yticks(fontsize=11)
+                # Crear el mapa de calor con Plotly Express
+                fig = px.imshow(correlation_matrix, labels=dict(x="Variables", y="Variables"),
+                                x=selected_columns, y=selected_columns, color_continuous_scale='Viridis',
+                                title=f"Mapa de Calor de Correlación ({selected_table})",
+                                zmin=-1, zmax=1, width=600, height=600)  # Ajusta zmin y zmax según tus necesidades
 
-                # Guardar el mapa de calor en la carpeta "static"
-                plt.savefig('static/image_correlacion/mapa_de_calor.png')
-                print("Mapa de calor guardado correctamente en 'static/image_correlacion/mapa_de_calor.png'")
+                # Añadir etiquetas de texto a cada celda
+                for i in range(len(selected_columns)):
+                    for j in range(len(selected_columns)):
+                        fig.add_annotation(
+                            x=selected_columns[j],
+                            y=selected_columns[i],
+                            text=f"{correlation_matrix.iloc[i, j]:.2f}",
+                            showarrow=False,
+                            font=dict(color='white' if -0.5 < correlation_matrix.iloc[i, j] < 0.5 else 'black')
+                        )
+
+                # Actualizar el diseño del mapa de calor
+                fig.update_layout(
+                    xaxis=dict(tickvals=list(range(len(selected_columns))), ticktext=selected_columns),
+                    yaxis=dict(tickvals=list(range(len(selected_columns))), ticktext=selected_columns),
+                )
+
+                # Guardar el mapa de calor en formato HTML
+                fig.write_html("static/image_correlacion/mapa_de_calor.html")
+                print("Mapa de calor guardado correctamente en 'static/image_correlacion/mapa_de_calor.html'")
 
                 # Crear un mensaje para mostrar las dos variables con la correlación más alta
-                message = f"Las dos variables con la correlación más alta son: {variable1} y {variable2}. El coeficiente de correlación es {highest_corr}."
+                message = f"Las dos variables con la correlación más alta son: <strong style='color: blue;'>{variable1}</strong> y <strong style='color: green;'>{variable2}</strong>. El coeficiente de correlación es <strong>{highest_corr}</strong>."
 
                 # Generar el mapa de calor y el mensaje como diccionario
                 result = {
                     "message": message,
-                    "heatmap_path": "static/image_correlacion/mapa_de_calor.png"  # Ruta de la imagen del mapa de calor
+                    "heatmap_path": "static/image_correlacion/mapa_de_calor.html"  # Ruta de la imagen del mapa de calor
                 }
 
                 # Devolver el resultado como un diccionario
